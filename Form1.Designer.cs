697eb4e3f7e082ecc63e6e6891bf85ec8db718df
Tim <timmothy225@hotmail.com>
2017-02-09 12:01:25 +0100

    added 4 random ducks

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,20 +28,95 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbWinChance1 = new System.Windows.Forms.Label();
+            this.lbDuck1 = new System.Windows.Forms.Label();
+            this.lbDuck2 = new System.Windows.Forms.Label();
+            this.lbDuck3 = new System.Windows.Forms.Label();
+            this.lbDuck4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(73, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Win chance in one game";
+            // 
+            // lbWinChance1
+            // 
+            this.lbWinChance1.AutoSize = true;
+            this.lbWinChance1.Location = new System.Drawing.Point(128, 97);
+            this.lbWinChance1.Name = "lbWinChance1";
+            this.lbWinChance1.Size = new System.Drawing.Size(13, 13);
+            this.lbWinChance1.TabIndex = 2;
+            this.lbWinChance1.Text = "0";
+            // 
+            // lbDuck1
+            // 
+            this.lbDuck1.AutoSize = true;
+            this.lbDuck1.Location = new System.Drawing.Point(54, 143);
+            this.lbDuck1.Name = "lbDuck1";
+            this.lbDuck1.Size = new System.Drawing.Size(35, 13);
+            this.lbDuck1.TabIndex = 3;
+            this.lbDuck1.Text = "label2";
+            // 
+            // lbDuck2
+            // 
+            this.lbDuck2.AutoSize = true;
+            this.lbDuck2.Location = new System.Drawing.Point(54, 169);
+            this.lbDuck2.Name = "lbDuck2";
+            this.lbDuck2.Size = new System.Drawing.Size(35, 13);
+            this.lbDuck2.TabIndex = 4;
+            this.lbDuck2.Text = "label3";
+            // 
+            // lbDuck3
+            // 
+            this.lbDuck3.AutoSize = true;
+            this.lbDuck3.Location = new System.Drawing.Point(54, 195);
+            this.lbDuck3.Name = "lbDuck3";
+            this.lbDuck3.Size = new System.Drawing.Size(35, 13);
+            this.lbDuck3.TabIndex = 5;
+            this.lbDuck3.Text = "label4";
+            // 
+            // lbDuck4
+            // 
+            this.lbDuck4.AutoSize = true;
+            this.lbDuck4.Location = new System.Drawing.Point(54, 220);
+            this.lbDuck4.Name = "lbDuck4";
+            this.lbDuck4.Size = new System.Drawing.Size(35, 13);
+            this.lbDuck4.TabIndex = 6;
+            this.lbDuck4.Text = "label5";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lbDuck4);
+            this.Controls.Add(this.lbDuck3);
+            this.Controls.Add(this.lbDuck2);
+            this.Controls.Add(this.lbDuck1);
+            this.Controls.Add(this.lbWinChance1);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Ducks & chance to win";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbWinChance1;
+        private System.Windows.Forms.Label lbDuck1;
+        private System.Windows.Forms.Label lbDuck2;
+        private System.Windows.Forms.Label lbDuck3;
+        private System.Windows.Forms.Label lbDuck4;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,53 @@ namespace Ducks
 {
     public partial class Form1 : Form
     {
+        int _chance = 0;
+        int _chance2 = 0;
+        int _beurd = 1;
+        bool _price = false;
+        int _duck1 = 0;
+        int _duck2 = 0;
+        int _duck3 = 0;
+        int _duck4 = 0;
+       
+
         public Form1()
         {
+            
             InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            RandomDuck();
+            lbDuck1.Text = _duck1.ToString();
+            lbDuck2.Text = _duck2.ToString();
+            lbDuck3.Text = _duck3.ToString();
+            lbDuck4.Text = _duck4.ToString();
+        }
+        public void Play()
+        {
+            _beurd++;
+
+        }
+        public void RandomDuck()
+        {
+            Random random1 = new Random();
+            int randomNumber1 = random1.Next(1, 14);
+            _duck1 = randomNumber1;
+
+            Random random2 = new Random();
+            int randomNumber2 = random1.Next(1, 14);
+            _duck2 = randomNumber2;
+
+            Random random3 = new Random();
+            int randomNumber3 = random1.Next(1, 14);
+            _duck3 = randomNumber3;
+
+            Random random4 = new Random();
+            int randomNumber4 = random1.Next(1, 14);
+            _duck4 = randomNumber4;
         }
     }
 }
